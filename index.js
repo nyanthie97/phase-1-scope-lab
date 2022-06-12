15faec9b8fc28786ac859fc387240eabe3138795
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-12 03:11:00 +0300

    commit tree dc7f738c0493510834c36c12756522ac6532d4d2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'dsgjfsgf'
+function upperCaseCustomerName() {
+     customerName = customerName.toUpperCase()
+};
+
+function setBestCustomer() {
+      bestCustomer = 'not bob' //why can i use a var declaration  here? 
+};
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+
+};
+
+function changeLeastFavoriteCustomer()  {
+     leastFavoriteCustomer = 'oniadfgnkiafg'
+
+};
